16e25b94eacc0cafe4bdcdc38ffc22f02bd69570
Manoj Agarwal <68643051+manoj7612@users.noreply.github.com>
2025-06-23 12:11:51 +0530

    Create pass_variables_using_tfvars.tf
    A main.tf file that shows use of terraform.tfvars file to pass two parameters named group_name and location to this script.

---
+++ examples/ex001/pass_variables_using_tfvars.tf
@@ -0,0 +1,32 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable group_name {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where the resource group will be created"
+  type        = string
+}
+
+resource "azurerm_resource_group" "RGVar01" {
+  name     = var.group_name
+  location = var.location
+  
+}
+
